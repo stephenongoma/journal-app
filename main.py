511c4680da9758702a648451d171e99b59f244d9
stephenongoma <odrumak@gmail.com>
2026-03-03 12:19:12 +0300

    Session 2: connect to PostgreSQL and add user registration endpoint

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db, engine
+import models, schemas
+from passlib.context import CryptContext
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 @app.get("/")
-def home():
-    return {"message": "Welcome to my Journal App!"}
+def read_root():
+    return {"message": "Journal App API is running!"}
 
-@app.get("/hello/{name}")
-def hello (name: str):
-    return {"message": f"Hello, {name}!"}
+@app.post("/auth/register", response_model=schemas.UserResponse)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Email already registered")
 
+    hashed_password = pwd_context.hash(user.password)
+
+    new_user = models.User(
+        username=user.username,
+        email=user.email,
+        password_hash=hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
